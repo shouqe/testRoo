6df9944ed80e6e75ec61fe6d80266ac6078d7b9b
gary li <zhendeshiwo@gmail.com>
2012-05-23 10:22:51 +0800

    first version

---
+++ src/main/java/com/dw/roo/conference/domain/Talk.java
@@ -0,0 +1,25 @@
+package com.dw.roo.conference.domain;
+
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Talk {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    @Size(max = 4000)
+    private String description;
+
+    @NotNull
+    @ManyToOne
+    private Speaker speaker;
+}
